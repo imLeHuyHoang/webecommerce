04bc4dc0de10c73ec1b1a1e90ef575220cf2f328
Le Huy Hoang <lehuyhoang1352002@gmail.com>
2024-12-10 11:24:45 +0700

    213123
    123

--- web-backend/index.js
+++ web-backend/index.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://54.169.183.168:5173"],
-
+    // cros ["http://54.169.183.168:5173"],[ "http://localhost:5173"],
+    origin: ["http://54.169.183.168:5173", "http://localhost:5173"],
     credentials: true, // Cho phép gửi cookies
   })
 );
