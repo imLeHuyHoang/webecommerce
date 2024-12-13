ab95880d923fafa35249a1bfdde15a9414c79a68
Le Huy Hoang <lehuyhoang1352002@gmail.com>
2024-12-13 09:31:49 +0700

    123
    123

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Validating service..."
 # Kiểm tra xem ứng dụng đã chạy đúng chưa, ví dụ kiểm tra HTTP response
-curl -f http://localhost:5000 || exit 1
-curl -f http://localhost:5173 || exit 1
+
