7c3b38018726ea7f5a7a8a9664cd1d755be40160
Le Huy Hoang <lehuyhoang1352002@gmail.com>
2024-12-15 08:57:13 +0700

    123321
    34243

--- web-frontend/vite.config.js
+++ web-frontend/vite.config.js
@@ -1,11 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    },
+  },
   server: {
     host: "0.0.0.0",
-    port: 5173,
+    port: 5173, // Giữ nguyên cổng 5173
   },
 });
