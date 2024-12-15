217f1b5adfb04feacc18d503d64b76d08fe20baf
Le Huy Hoang <lehuyhoang1352002@gmail.com>
2024-12-15 07:46:36 +0700

    123
    123

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -5,18 +5,14 @@ set -e  # Dừng script nếu có lỗi xảy ra
 
 echo "Starting application..."
 
-# Thiết lập thư mục ứng dụng
+
 APP_DIR="/home/ec2-user/myapp"
 
-# Chuyển đến thư mục ứng dụng
 echo "Navigating to application directory: $APP_DIR"
 cd $APP_DIR
 
-# Khởi động lại các container với cấu hình mới
-echo "Stopping existing Docker containers..."
 docker-compose down
 
-echo "Starting Docker containers in detached mode..."
-docker-compose up --build -d
+docker-compose up -d
 
 echo "Application started successfully."
